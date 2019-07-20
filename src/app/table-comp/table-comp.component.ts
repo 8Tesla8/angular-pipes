380f1bff49fd15f4e6bf696699b09a9616d10e9b
tesla <kela256@gmail.com>
2019-07-20 16:45:26 +0300

    refactor

--- src/app/table-comp/table-comp.component.ts
+++ src/app/table-comp/table-comp.component.ts
@@ -8,17 +8,17 @@ import { Component } from '@angular/core';
 export class TableCompComponent {
 
   public data = {
+    percent: 0.15,
     money: 123.43,
-    numb: 0.15,
-    bigNumb: 23.456,
+    numb: 23.456,
     str: 'Hello User', 
     date: new Date(2015, 3, 25),
   } as Info;
 }
 
 export class Info {
+  public percent: number;
   public numb: number;
-  public bigNumb: number;
   public money: number;
 
   public str: string; 
